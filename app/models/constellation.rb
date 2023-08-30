e48a36e24abec2b107402743bac02de11b4520df
pcbennett108 <pcbennett42@gmail.com>
2023-08-30 10:20:57 -0600

    re introduce error for demo

--- app/models/constellation.rb
+++ app/models/constellation.rb
@@ -1,5 +1,7 @@
 class Constellation < ApplicationRecord
   has_many :stars
+  # has_many :patients, dependent: :destroy
+  # Allows automatic deletion of childern when parent deleted.
 end
 
 def distance_greater_than(amount)
